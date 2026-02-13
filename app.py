3ebd945d3703a35051f5331fa785c7e19e55874f
Marvin <marvin.poppe@esders.de>
2026-02-13 08:18:17 +0100

    Neu Nachricht

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World! \nThis is a simple Flask application running in a Docker container.'
 
 if __name__ == '__main__':
     app.run(debug=True)
